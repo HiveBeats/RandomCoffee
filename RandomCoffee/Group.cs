9b7abc168de083d6afd388067014cb289c08fa0d
Лалетин Иван Антонович <i.laletin@tinkoff.ru>
2025-01-30 10:34:59 +0300

    fix: исправление опечатки и добавление методов для управления участниками и кофе-встречами
    - Исправлена опечатка в свойстве `CoffeeId` в классе `Group`.
    - Добавлен конструктор и методы для управления участниками и кофе-встречами в классах `Coffee` и `Group`.
    - Добавлен метод `AddParticipant` для добавления участников в кофе.
    - Добавлен метод `AddCoffee` для создания новой кофе-встречи в группе.
    - Добавлен метод `AddParticipant` для добавления участников в группу с проверкой на пустое имя.
    - Добавлен метод `AnnounceCoffee` для объявления кофе-встречи и формирования групп участников.
    - Инициализированы списки `CoffeeParticipants` и `Coffees` по умолчанию.

--- RandomCoffee/Group.cs
+++ RandomCoffee/Group.cs
@@ -9,27 +9,104 @@ public class Participant
         Id = id;
         UserName = username;
         ScheduledAt = DateTime.UtcNow;
-        CoffeId = coffee.Id;
+        CoffeeId = coffee.Id;
     }
     
     public string Id { get; set; }
     public string UserName { get; set; }
     public DateTime ScheduledAt { get; set; }
-    public Guid CoffeId { get; set; }
+    public Guid CoffeeId { get; set; }
     public Coffee Coffee { get; set; }
 }
 
 public class Coffee
 {
+    public Coffee(Group group)
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        GroupId = Group.Id;
+    }
     public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime? AnnouncedAt { get; set; }
-    public List<Participant> CoffeeParticipants { get; set; }
+    public List<Participant> CoffeeParticipants { get; set; } = new List<Participant>();
     public string GroupId { get; set; }
     public Group Group { get; set; }
+
+    public void AddParticipant(Participant participant)
+    {
+        CoffeeParticipants.Add(participant);
+    }
 }
 
 public class Group
 {
-    public string Id { get; set; } //chatId
-    public List<Coffee> Coffees { get; set; }    
+    public Group(string chatId)
+    {
+        Id = chatId;
+    }
+    
+    public string Id { get; set; }
+    public List<Coffee> Coffees { get; set; } = new List<Coffee>();
+
+    public void AddCoffee()
+    {
+        Coffees.Add(new Coffee(this));
+    }
+
+    public void AddParticipant(string username)
+    {
+        if (string.IsNullOrWhiteSpace(username))
+            throw new ArgumentException("Username cannot be null or empty.", nameof(username));
+        
+        var coffee = Coffees.LastOrDefault(x => x.AnnouncedAt == null);
+        if (coffee == null)
+            throw new ApplicationException("Ты не успел подписаться на текущую встречу. В понедельник я объявлю сбор на следующую, не пропусти!");
+        coffee.AddParticipant(new Participant(Guid.NewGuid().ToString(), username, coffee));
+    }
+    
+    private List<List<Participant>> GroupParticipants(List<Participant> participants)
+    {
+        int count = participants.Count;
+        if (count == 0)
+            return [];
+        
+        // If the number of participants is even, we need to group them into two groups
+        if (count % 2 == 0)
+        {
+            return MakePairs(participants);
+        }
+
+        var evenParticipants = participants.Take(count - 1).ToList();
+        var oddParticipant = participants.Skip(count - 1).First();
+        var evenGroups = MakePairs(evenParticipants);
+            
+        evenGroups.Last().Add(oddParticipant);
+
+        return evenGroups;
+
+        List<List<Participant>> MakePairs(IEnumerable<Participant> guys)
+        {
+            return guys
+                .Select((participant, index) => new { Participant = participant, Index = index })
+                .GroupBy(x => x.Index / 2) // Grouping by index divided by 2
+                .Select(g => g.Select(x => x.Participant).ToList()) // Selecting participants from groups
+                .ToList();
+        }
+    }
+
+    public string? AnnounceCoffee()
+    {
+        var coffee = Coffees.LastOrDefault(x => x.AnnouncedAt == null);
+        if (coffee == null)
+            return null;
+        
+        coffee.AnnouncedAt = DateTime.UtcNow;
+        
+        var groups = GroupParticipants(coffee.CoffeeParticipants);
+        var groupStrings = string.Join(Environment.NewLine, groups.Select(x => string.Join(" \u2615\ufe0f ", x.Select(y => $"@{y.UserName}"))));
+        return
+            $"Появилась информация о кофе-встречах.\nГруппы для кофе готовы. Назначайте встречи:\n _Встречу назначает человек слева_\n{groupStrings}";
+    }
 }
